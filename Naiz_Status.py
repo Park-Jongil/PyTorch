ffe9f6e8ff06a9d3f32a47b1f5c4b6fb933bde43
Park-Jongil <41685875+Park-Jongil@users.noreply.github.com>
2018-09-03 17:15:39 +0900

    Add files via upload

--- Naiz_Status.py
+++ Naiz_Status.py
@@ -1,39 +1,86 @@
 import urllib.request
 import xml.etree.ElementTree as ET
+import sqlite3
+from sqlite3 import Error
 
-naiz_url = 'http://naiz.re.kr:8001/event/status.cgi?id=admin&password=admin&key=all&method=get'
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+ 
+    return None
 
-file = urllib.request.urlopen( naiz_url ).read().decode('euc-kr')
-root = ET.fromstring(file)
-iCount = 0
-isAlive = 0
+def select_status_by_key(conn, key):
+    """
+    Query tasks by priority
+    :param conn: the Connection object
+    :param priority:
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT seq,status FROM CameraList WHERE seq=?", (key,))
+    row = cur.fetchone()
+    return row[1]
 
-for child in root :
-    for sub in child :
-        HighStreamConnection = '0'
-        LowStreamConnection  = '0'
-        for item in sub :
-            if (item.tag == 'Key') :      
-                UniqueKey = item.text
-            if (item.tag == 'AlarmIn') :      
-                AlarmIn = item.text
-            if (item.tag == 'Record') :      
-                Record = item.text
-            if (item.tag == 'HighStreamConnection') :    
-                HighStreamConnection = item.text  
-            if (item.tag == 'LowStreamConnection') :      
-                LowStreamConnection = item.text  
-        print("UniqueKey = " + UniqueKey)
-        print("AlarmIn = " + AlarmIn)
-        print("Record = " + Record)
-        print("HighStreamConnection  = " + HighStreamConnection)
-        print("LowStreamConnection   = " + LowStreamConnection)
-        if (HighStreamConnection=='1') and (LowStreamConnection=='1')  :
-            isAlive = isAlive + 1   
-        iCount = iCount + 1
-
-print("\n전체갯수 = " + str(iCount))
-print("\n활성화   = " + str(isAlive))
+def update_status_by_key(conn, key , status):
+    """
+    Query tasks by priority
+    :param conn: the Connection object
+    :param priority:
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("UPDATE CameraList SET status=? WHERE seq=?", (status, key))        
+    conn.commit()
 
 
+def main():
+    database = "NaizDB.db"
+    naiz_url = 'http://naiz.re.kr:8001/event/status.cgi?id=admin&password=admin&key=all&method=get'
+
+    conn = create_connection(database)
+
+    file = urllib.request.urlopen( naiz_url ).read().decode('euc-kr')
+    root = ET.fromstring(file)
+    iCount = 0
+    isAlive = 0
+    iPrevStatus = 0
+
+    for child in root :
+        for sub in child :
+            HighStreamConnection = '0'
+            LowStreamConnection  = '0'
+            isCheckAlive = 0
+            for item in sub :
+                if (item.tag == 'Key') :      
+                    UniqueKey = item.text
+                if (item.tag == 'HighStreamConnection') :    
+                    HighStreamConnection = item.text  
+                if (item.tag == 'LowStreamConnection') :      
+                    LowStreamConnection = item.text  
+            print("UniqueKey = " + UniqueKey)
+            print("HighStreamConnection  = " + HighStreamConnection)
+            print("LowStreamConnection   = " + LowStreamConnection)
+            if (HighStreamConnection=='1') and (LowStreamConnection=='1')  :
+                isAlive = isAlive + 1   
+                isCheckAlive = 1
+            iPrevStatus = select_status_by_key( conn , int(UniqueKey) )
+            if (iPrevStatus != isCheckAlive) :
+                print(" 상태값 변이가 발생함 ")
+                update_status_by_key( conn , int(UniqueKey) , isCheckAlive )
+            iCount = iCount + 1
+
+    print("\n전체갯수 = " + str(iCount))
+    print("\n활성화   = " + str(isAlive))
+    conn.close()
+
+if __name__ == '__main__':
+    main()
 
