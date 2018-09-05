a76c1a0f5de9c5786cb9934154101232e33505db
Park-Jongil <41685875+Park-Jongil@users.noreply.github.com>
2018-09-05 17:27:56 +0900

    Add files via upload

--- NaizList.py
+++ NaizList.py
@@ -5,11 +5,6 @@ from sqlite3 import Error
 
 
 def create_connection(db_file):
-    """ create a database connection to the SQLite database
-        specified by the db_file
-    :param db_file: database file
-    :return: Connection object or None
-    """
     try:
         conn = sqlite3.connect(db_file)
         return conn
@@ -19,21 +14,16 @@ def create_connection(db_file):
     return None
 
 def select_name_by_key(conn, key):
-    """
-    Query tasks by priority
-    :param conn: the Connection object
-    :param priority:
-    :return:
-    """
     cur = conn.cursor()
-    cur.execute("SELECT seq,name FROM CameraList WHERE seq=?", (key,))
+    cur.execute("select seq,name,ip_addr from CameraList where seq=?",(int(key),) )
     row = cur.fetchone()
+    if (row==None) : return None
     return row[1]
 
 def main():
     database = "NaizDB.db"
 
-    naiz_url = 'http://naiz.re.kr:8001/camera/list.cgi?id=admin&password=admin&key=all&method=get'
+    naiz_url = 'http://172.18.200.36:80/camera/list.cgi?id=admin&password=spdlwm1234&key=all&method=get'
     conn = create_connection(database)
     cur = conn.cursor()
 
@@ -58,10 +48,10 @@ def main():
     # sqlite test.db 에 해당내용 저장        
             try :
                 print("UniqueKey = " + UniqueKey)
-                findname = select_name_by_key( conn , int(UniqueKey) )
-                if (findname != Name) :
-                    sql_stmt = "insert into CameraList(seq,name,ip_addr,rtsp_url1,rtsp_url2,status) values(?,?,?,?,?,0)"
-                    cur.execute( sql_stmt,(int(UniqueKey),Name,IP_Addr,RTSP_URL1,RTSP_URL2))
+                findname = select_name_by_key( conn , UniqueKey )
+                if (findname==None) or (findname != Name) :
+                    sql_stmt = "insert into CameraList(seq,name,ip_addr,rtsp_url1,rtsp_url2,status) values(?,?,?,?,?,?)"
+                    cur.execute( sql_stmt,(int(UniqueKey),Name,IP_Addr,RTSP_URL1,RTSP_URL2,0))
                     conn.commit()
                     print("Name = " + Name)
                     print("Address = " + IP_Addr)
@@ -78,3 +68,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
